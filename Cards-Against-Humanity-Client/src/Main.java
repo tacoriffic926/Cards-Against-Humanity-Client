d0a5e3f47fe5c7ab37c722747286835b87a5f44e
TheStickMeister <sticky.boy97@yahoo.com>
2015-01-24 21:45:51 -0500

    I did it

--- Cards-Against-Humanity-Client/src/Main.java
+++ Cards-Against-Humanity-Client/src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Justin, We made it!");
 	}
 
 }
