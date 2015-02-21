fe52e911f9b37af2745218fc3a51c569343c28db
TheStickMeister <sticky.boy97@yahoo.com>
2015-02-21 10:26:16 -0500

    added window to server

--- CAH_Server/src/Server.java
+++ CAH_Server/src/Server.java
@@ -1,9 +1,18 @@
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 
 
@@ -11,21 +20,43 @@ import java.util.logging.Logger;
  *
  * @author Nick and Justin
  */
-public class Server 
+
+public class Server extends JFrame implements ActionListener
 {
     ArrayList<ServerClient> clients = new ArrayList();
     
     boolean gameInProgress, running;
     private int port; 
     private ServerSocket server;
+    private JTextArea output;
+    private JTextField cmdLine;
+    private Font font = new Font("Helvetica", Font.BOLD, 12);
     
     public Server(int port)
     {
         this.port = port;
+        
+        cmdLine = new JTextField();
+        cmdLine.setFont(font);
+        cmdLine.setBackground(Color.black);
+        cmdLine.setForeground(Color.white);
+        cmdLine.addActionListener(this);
+        add(cmdLine, BorderLayout.SOUTH);
+        
+        output = new JTextArea();
+        output.setFont(font);
+        output.setBackground(Color.black);
+        output.setForeground(Color.white);
+        output.setEditable(false);
+        add(new JScrollPane(output));
+        setSize(600,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
     }
     
     public void start()
     {
+        setVisible(true);
         try 
         {
             server = new ServerSocket(port);
@@ -47,6 +78,8 @@ public class Server
         {
             while(!gameInProgress)  //Check for connections, Pick Card Packs, etc...
             {
+                if(!running)
+                    break;
                 try 
                 {
                     newClient = new ServerClient(server.accept(), this);
@@ -63,8 +96,32 @@ public class Server
             
             while(gameInProgress) //Game Logic & Stuff
             {
-                
+                if(!running)
+                    break;
             }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        String command = e.getActionCommand();
+        if(!command.equals(""))
+        {
+            if(command.charAt(0) == '/')
+            {
+                if(e.getActionCommand().equals("/stop"))
+                {
+                    output.append("Server Shutting Down\n");
+                    running = false;
+//                    stop();
+                }
+            }
+            else
+            {
+                output.append(command + "\n");
+            }
+            cmdLine.setText("");
         }
     }
 }
